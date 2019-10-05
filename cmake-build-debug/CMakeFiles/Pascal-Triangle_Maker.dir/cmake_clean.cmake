26c0d7fc6b00d9ce37c647fc6cc5ec0dccb37f47
Alejandro Rayas <alexrayas@rocketmail.com>
2019-10-05 16:32:45 -0700

    First
    First attempt upload

---
+++ cmake-build-debug/CMakeFiles/Pascal-Triangle_Maker.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pascal-Triangle_Maker.dir/main.cpp.o"
+  "Pascal-Triangle_Maker.pdb"
+  "Pascal-Triangle_Maker.exe"
+  "libPascal-Triangle_Maker.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pascal-Triangle_Maker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
